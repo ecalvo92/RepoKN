33ffa922d019c3967b732f858445d58e10bf1289
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-09-19 18:24:40 -0600

    Versión Inicial

---
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_ProyectoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Esta es la página de acerca de...";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+    }
+}
