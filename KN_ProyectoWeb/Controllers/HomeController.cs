e152144179bd100ca40584cd652cb25fe970f47f
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-09-23 20:49:26 -0600

    Fin de semana 3

--- KN_ProyectoWeb/Controllers/HomeController.cs
+++ KN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,24 +1,65 @@
-﻿using System.Web.Mvc;
+﻿using KN_ProyectoWeb.Models;
+using System.Web.Mvc;
 
 namespace KN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
+        #region Iniciar Sesión
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            /*Progra para validar si usuario.CorreoElectronico y usuario.Contrasenna*/
+
+            return RedirectToAction("Principal", "Home");
+        }
+
+        #endregion
+
+        #region Registro
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            /*Progra para ir a guardar el usuario en la BD*/
+
+            return View();
+        }
+
+        #endregion
+
+        #region Recuperar Acceso
+
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario)
+        {
+            /*Validar si el usuario existe, generarle un contraseña tmp, enviarle esa clave tmp*/
+
+            return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ KN_ProyectoWeb/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace KN_ProyectoWeb.Models
+{
+    public class Usuario
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasenna { get; set; }
+    }
+}
