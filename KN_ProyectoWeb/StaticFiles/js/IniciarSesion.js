88393904714af340d931c67e1a9aed9a2cc40a3d
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-10-07 20:23:04 -0600

    Jquery Validate Formularios

---
+++ KN_ProyectoWeb/StaticFiles/js/IniciarSesion.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+
+    $("#FormIniciarSesion").validate({
+        rules: {
+            CorreoElectronico: {
+                required: true,
+                email: true
+            },
+            Contrasenna: {
+                required: true
+            }
+        },
+        messages: {
+            CorreoElectronico: {
+                required: "* Requerido",
+                email: "* Formato",
+            },
+            Contrasenna: {
+                required: "* Requerido",
+            }
+        }
+    });
+
+});
--- KN_ProyectoWeb/StaticFiles/js/Registro.js
+++ KN_ProyectoWeb/StaticFiles/js/Registro.js
@@ -16,3 +16,40 @@
 
     }
 }
+
+$(function () {
+
+    $("#FormRegistro").validate({
+        rules: {
+            Identificacion: {
+                required: true
+            },
+            Nombre: {
+                required: true
+            },
+            CorreoElectronico: {
+                required: true,
+                email:true
+            },
+            Contrasenna: {
+                required: true
+            }
+        },
+        messages: {
+            Identificacion: {
+                required: "* Requerido",
+            },
+            Nombre: {
+                required: "* Requerido",
+            },
+            CorreoElectronico: {
+                required: "* Requerido",
+                email: "* Formato",
+            },
+            Contrasenna: {
+                required: "* Requerido",
+            }
+        }
+    });
+
+});
