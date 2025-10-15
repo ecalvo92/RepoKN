04faca42f8c2be36ba93b0cbf8d449ed96f468fe
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-10-14 21:05:37 -0600

    Update CREATE DB.sql

--- CREATE DB.sql
+++ CREATE DB.sql
@@ -7,12 +7,37 @@ GO
 USE [BD_KN]
 GO
 
+CREATE TABLE [dbo].[tbCategoria](
+	[ConsecutivoCategoria] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_tbCategoria] PRIMARY KEY CLUSTERED 
+(
+	[ConsecutivoCategoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 CREATE TABLE [dbo].[tbPerfil](
 	[ConsecutivoPerfil] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](100) NOT NULL,
  CONSTRAINT [PK_tbPerfil] PRIMARY KEY CLUSTERED 
 (
 	[ConsecutivoPerfil] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[tbProducto](
+	[ConsecutivoProducto] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[Descripcion] [varchar](2000) NOT NULL,
+	[Precio] [decimal](10, 2) NOT NULL,
+	[ConsecutivoCategoria] [int] NOT NULL,
+	[Estado] [bit] NOT NULL,
+	[Imagen] [varchar](250) NOT NULL,
+ CONSTRAINT [PK_tbProducto] PRIMARY KEY CLUSTERED 
+(
+	[ConsecutivoProducto] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -32,6 +57,29 @@ CREATE TABLE [dbo].[tbUsuario](
 ) ON [PRIMARY]
 GO
 
+SET IDENTITY_INSERT [dbo].[tbCategoria] ON 
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (1, N'Computadoras y componentes')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (2, N'Celulares y accesorios')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (3, N'Audio y entretenimiento')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (4, N'Redes y conectividad')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (5, N'Hogar inteligente (Smart Home)')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (6, N'Impresoras y consumibles')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (7, N'Accesorios y gadgets')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (8, N'Tecnología para oficina y punto de venta')
+GO
+INSERT [dbo].[tbCategoria] ([ConsecutivoCategoria], [Nombre]) VALUES (9, N'Robótica y proyectos educativos')
+GO
+SET IDENTITY_INSERT [dbo].[tbCategoria] OFF
+GO
+
 SET IDENTITY_INSERT [dbo].[tbPerfil] ON 
 GO
 INSERT [dbo].[tbPerfil] ([ConsecutivoPerfil], [Nombre]) VALUES (1, N'Usuario Administrador')
@@ -39,6 +87,13 @@ GO
 INSERT [dbo].[tbPerfil] ([ConsecutivoPerfil], [Nombre]) VALUES (2, N'Usuario Regular')
 GO
 SET IDENTITY_INSERT [dbo].[tbPerfil] OFF
+GO
+
+SET IDENTITY_INSERT [dbo].[tbProducto] ON 
+GO
+INSERT [dbo].[tbProducto] ([ConsecutivoProducto], [Nombre], [Descripcion], [Precio], [ConsecutivoCategoria], [Estado], [Imagen]) VALUES (1, N'Play Station 5', N'La consola de nueva generación de Sony que ofrece gráficos de alta fidelidad, carga ultrarrápida gracias al SSD, y una experiencia de juego inmersiva con el innovador control DualSense. Compatible con títulos exclusivos, juegos en 4K y tecnología de trazado de rayos (Ray Tracing). Ideal para gamers que buscan rendimiento y entretenimiento de última generación.', CAST(525.00 AS Decimal(10, 2)), 3, 1, N'https://i2.wp.com/techstomper.com/wp-content/uploads/2020/06/PS5-Horizontal-e1592741758862.jpg?fit=2398%2C1348&ssl=1')
+GO
+SET IDENTITY_INSERT [dbo].[tbProducto] OFF
 GO
 
 SET IDENTITY_INSERT [dbo].[tbUsuario] ON 
@@ -49,23 +104,27 @@ INSERT [dbo].[tbUsuario] ([ConsecutivoUsuario], [Identificacion], [Nombre], [Cor
 GO
 INSERT [dbo].[tbUsuario] ([ConsecutivoUsuario], [Identificacion], [Nombre], [CorreoElectronico], [Contrasenna], [Estado], [ConsecutivoPerfil]) VALUES (3, N'304590417', N'Alex Cesar Fajardo', N'ecalvo90417@ufide.ac.cr', N'12313221', 1, 2)
 GO
-INSERT [dbo].[tbUsuario] ([ConsecutivoUsuario], [Identificacion], [Nombre], [CorreoElectronico], [Contrasenna], [Estado], [ConsecutivoPerfil]) VALUES (4, N'119780659', N'IGNACIO AGUILAR FERNANDEZ', N'iaguilar80659@ufide.ac.cr', N'80659', 1, 2)
+INSERT [dbo].[tbUsuario] ([ConsecutivoUsuario], [Identificacion], [Nombre], [CorreoElectronico], [Contrasenna], [Estado], [ConsecutivoPerfil]) VALUES (4, N'119780659', N'IGNACIO AGUILAR FERNANDEZ', N'iaguilar80659@ufide.ac.cr', N'W8O15QK4', 1, 2)
 GO
 SET IDENTITY_INSERT [dbo].[tbUsuario] OFF
 GO
 
-/****** Object:  Index [UK_CorreoElectronico]    Script Date: 7/10/2025 20:36:53 ******/
 ALTER TABLE [dbo].[tbUsuario] ADD  CONSTRAINT [UK_CorreoElectronico] UNIQUE NONCLUSTERED 
 (
 	[CorreoElectronico] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 
-/****** Object:  Index [UK_Identificacion]    Script Date: 7/10/2025 20:36:53 ******/
 ALTER TABLE [dbo].[tbUsuario] ADD  CONSTRAINT [UK_Identificacion] UNIQUE NONCLUSTERED 
 (
 	[Identificacion] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tbProducto]  WITH CHECK ADD  CONSTRAINT [FK_tbProducto_tbCategoria] FOREIGN KEY([ConsecutivoCategoria])
+REFERENCES [dbo].[tbCategoria] ([ConsecutivoCategoria])
+GO
+ALTER TABLE [dbo].[tbProducto] CHECK CONSTRAINT [FK_tbProducto_tbCategoria]
 GO
 
 ALTER TABLE [dbo].[tbUsuario]  WITH CHECK ADD  CONSTRAINT [FK_tbUsuario_tbPerfil] FOREIGN KEY([ConsecutivoPerfil])
@@ -117,4 +176,3 @@ BEGIN
 
 END
 GO
-
