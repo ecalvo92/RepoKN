fab18a7314eb629b6ff6f687cce659964250e2e0
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-05-19 20:37:55 -0600

    Versión inicial

---
+++ KN_WEB/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+            Action = Eventos disparados por el usuario desde una vista
+            Método = Funciones pública o privada Centralizar Código y Reutilizarlo
+            Instancia = Trabajar con clases (objeto, entidad) externas
+            Tipos de datos = Trabajar con información
+        */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
