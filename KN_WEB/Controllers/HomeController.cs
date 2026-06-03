4dc5c1ea0e49268aca91edd97d610816fc902356
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-02 20:18:12 -0600

    Modelo, Razor, Acciones

--- KN_WEB/Controllers/HomeController.cs
+++ KN_WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KN_WEB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,23 +16,39 @@ namespace KN_WEB.Controllers
             Tipos de datos = Trabajar con información
         */
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        #region
 
+        [HttpGet]
         public ActionResult Registro()
         {
+            //Me permite abrir una vista, se dispara en un redireccionamiento o hipervínculo
+
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registro(UsuarioModel model)
+        {
+            //Me permite programar una acción en una vista, se dispara al presionar un botón de tipo 'submit'
 
+            return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ KN_WEB/Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KN_WEB.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasenna { get; set; }
+    }
+}
