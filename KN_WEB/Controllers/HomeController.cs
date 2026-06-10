eb07e9387d63c0ec4bdff69c8b2598f764319593
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-09 20:59:47 -0600

    Fin de Semana 5

--- KN_WEB/Controllers/HomeController.cs
+++ KN_WEB/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using KN_WEB.Models;
+﻿using KN_WEB.EF;
+using KN_WEB.Models;
+using KN_WEB.Servicios;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace KN_WEB.Controllers
@@ -16,28 +16,97 @@ namespace KN_WEB.Controllers
             Tipos de datos = Trabajar con información
         */
 
+        UtilitarioService utilitario = new UtilitarioService();
+
+        #region Autenticación de usuarios
+
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                utilitario.RegistrarErrorBitacora(ex.Message, "Registro", 0);
+                return View("Error");
+            }
         }
 
-        #region
+        [HttpPost]
+        public ActionResult Index(UsuarioModel model)
+        {
+            try
+            {
+                using (var context = new KN_BDEntities())
+                {
+                    var infoUsuario = (from U in context.tbUsuario
+                                       where U.CorreoElectronico == model.CorreoElectronico
+                                       && U.Contrasenna == model.Contrasenna
+                                       && U.Estado == true
+                                       select U).FirstOrDefault();
+
+                    if (infoUsuario == null)
+                    {
+                        return View();
+                    }
+
+                    return RedirectToAction("Principal", "Home");
+                }
+            }
+            catch (Exception ex)
+            {
+                utilitario.RegistrarErrorBitacora(ex.Message, "Registro", 0);
+                return View("Error");
+            }
+        }
+
+        #endregion
+
+        #region Registro de usuarios
 
         [HttpGet]
         public ActionResult Registro()
         {
-            //Me permite abrir una vista, se dispara en un redireccionamiento o hipervínculo
-
-            return View();
+            try
+            {
+                //Me permite abrir una vista, se dispara en un redireccionamiento o hipervínculo
+                return View();
+            }
+            catch (Exception ex)
+            {
+                utilitario.RegistrarErrorBitacora(ex.Message, "Registro", 0);
+                return View("Error");
+            }
         }
 
         [HttpPost]
         public ActionResult Registro(UsuarioModel model)
         {
-            //Me permite programar una acción en una vista, se dispara al presionar un botón de tipo 'submit'
+            try
+            {
+                //Me permite programar una acción en una vista, se dispara al presionar un botón de tipo 'submit'
+                using (var context = new KN_BDEntities())
+                {
+                    context.tbUsuario.Add(new tbUsuario
+                    {
+                        Identificacion = model.Identificacion,
+                        Nombre = model.Nombre,
+                        CorreoElectronico = model.CorreoElectronico,
+                        Contrasenna = model.Contrasenna,
+                        Estado = true
+                    });
 
-            return View();
+                    context.SaveChanges();
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                utilitario.RegistrarErrorBitacora(ex.Message, "Registro", 0);
+                return View("Error");
+            }
         }
 
         #endregion
